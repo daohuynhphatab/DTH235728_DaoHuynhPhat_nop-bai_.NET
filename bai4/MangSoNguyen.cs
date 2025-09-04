45875565158c72039715e30368c7d9d6047f4df5
N2C <N2C@DESKTOP-U6U8BT0>
2025-09-04 18:25:55 +0700

    Add project files.

---
+++ bai4/MangSoNguyen.cs
@@ -0,0 +1,86 @@
+﻿namespace Bai4
+{
+    class MangSoNguyen
+    {
+        //phuong thuc nhap mot so > 0
+        public int NhapSoDuong()
+        {
+            int so;
+            do
+            {
+                so = Convert.ToInt32(Console.ReadLine());
+            } while (so <= 0);
+            return so;
+        }
+        //phuong thuc nhap mot so
+        public int NhapMotSo()
+        {
+            int so;
+            so = Convert.ToInt32(Console.ReadLine());
+            return so;
+        }
+        //phuong thuc nhap mang
+        public void NhapMang(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("a[{0}] = ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        //phuong thuc in mang
+        public void InMang(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                Console.Write(" {0}", a[i]);
+        }
+        //so lon nhat
+        public int TimSoLonNhat(int[] a)
+        {
+            int max = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+            }
+            return max;
+        }
+        //so nho nhat
+        public int TimSoNhoNhat(int[] a)
+        {
+            int min = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+            }
+            return min;
+        }
+        //tinh tong
+        public int TinhTong(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+        //sap xep
+        public void SapXepTangDan(int[] a)
+        {
+            int temp;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < a[i]){
+                        temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
