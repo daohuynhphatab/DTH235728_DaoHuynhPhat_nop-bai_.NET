22376651738efbc2abddc9c6f3f902a4be923b88
N2C <N2C@DESKTOP-U6U8BT0>
2025-09-04 22:58:30 +0700

    message

--- bai6/Employee.cs
+++ bai6/Employee.cs
@@ -8,6 +8,54 @@ namespace bai6
 {
      class Employee
     {
+        int id;
+        string name;
+        int yearOfBirth;
+        double salaryLevel;
+        double basicSalary;
+        //Hàm khởi tạo không có tham số
+        public Employee()
+        {
+            id = 0;
+            name = "";
+            yearOfBirth = 0;
+            salaryLevel = 0;
+            basicSalary = 0;
+        }
+        //Hàm khởi tạo có tham số
+        public Employee(int id, string name, int yearOfBirth, double salaryLevel, double basicSalary)
+        {
+            this.id = id;
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+            this.salaryLevel = salaryLevel;
+            this.basicSalary = basicSalary;
+        }
+        //Các phương thức Properties để get/set giá trị cho các thuộc tính
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int YearOfBirth { get => yearOfBirth; set => yearOfBirth = value; }
+        public double Income { get => salaryLevel * basicSalary; }
+        public double SalaryLevel { get => salaryLevel; set => salaryLevel = value; }
+        public double BasicSalary { get => basicSalary; set => basicSalary = value; }
+        //Phương thức nhập/xuất dữ liệu
 
+        public void Input()
+        {
+            Console.Write(" \t -Nhap ma nhan vien: ");
+            Id = int.Parse(Console.ReadLine());
+            Console.Write(" \t -Nhap ho ten: ");
+            Name = Console.ReadLine();
+            Console.Write(" \t -Nhap nam sinh: ");
+            YearOfBirth = int.Parse(Console.ReadLine());
+            Console.Write(" \t -Nhap he so luong: ");
+            salaryLevel = double.Parse(Console.ReadLine());
+            Console.Write(" \t -Nhap luong co ban: ");
+            basicSalary = double.Parse(Console.ReadLine());
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}", Id, Name, YearOfBirth, Income);
+        }
     }
 }
--- bai6/Program.cs
+++ bai6/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bai6;
+
+class program
+{
+    static void Main(string[] args)
+    {
+       Employee employee = new Employee();
+        Console.Write(" Nhap id Nhan vien: ");
+        employee.Id = int.Parse(Console.ReadLine());
+        Console.Write(" Nhap ten Nhan vien: ");
+        employee.Name = Console.ReadLine();
+        Console.Write(" Nhap nam sinh Nhan vien: ");
+        employee.YearOfBirth = int.Parse(Console.ReadLine());
+        Console.Write(" Nhap he so luong Nhan vien: ");
+        employee.SalaryLevel = double.Parse(Console.ReadLine());
+        Console.Write(" Nhap luong co ban Nhan vien: ");
+        employee.BasicSalary = double.Parse(Console.ReadLine());
+        Console.WriteLine(" Thong tin Nhan vien: ");
+        Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}", "ID", "Ten", "Nam Sinh", "Thu Nhap");
+        employee.Display();
+        Console.ReadLine();
+    }
+}
